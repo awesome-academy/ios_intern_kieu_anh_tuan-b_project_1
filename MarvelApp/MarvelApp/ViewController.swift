080dc339e0a9bf966e9cef20dfaa7150c009c20c
Kiều Anh Tuấn <tuanka@rocketstudio.com.vn>
2023-09-12 08:28:54 +0700

    init project with swiftlint

---
+++ MarvelApp/MarvelApp/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  MarvelApp
+//
+//  Created by Tobi on 11/09/2023.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
